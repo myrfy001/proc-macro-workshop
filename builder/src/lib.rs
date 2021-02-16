1d727f47407b97b8b3b63f694fd156d698b271ef
myrfy001 <myrfy001@users.noreply.github.com>
2021-02-16 17:47:10 +0800

    builder-05-finished

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,85 @@
+
+use proc_macro2;
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
+
+use std::error::Error;
+
+mod utils;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    
+    let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("{:#?}", ast);
 
-    unimplemented!()
+    let struct_name = &ast.ident;
+    let builder_struct_name = format_ident!("{}Builder", struct_name);
+
+    let field_names:Vec<syn::Ident> = utils::derive_get_struct_fields(&ast).unwrap().iter().map(|field|{
+        field.ident.clone().unwrap()
+    }).collect();
+
+    let field_types:Vec<syn::Type> = utils::derive_get_struct_fields(&ast).unwrap().iter().map(|field|{
+        field.ty.clone()
+    }).collect();
+
+    let field_mising_err_msg:Vec<String> =  utils::derive_get_struct_fields(&ast).unwrap().iter().map(|field|{
+        format!("Field {} is missing",field.ident.clone().unwrap())
+    }).collect();
+
+
+    let builder_struct_def = quote! {
+        
+        pub struct #builder_struct_name {
+            #(#field_names:Option<#field_types>),*
+        }
+
+        impl #struct_name {
+            pub fn builder() -> #builder_struct_name {
+                #builder_struct_name {
+                    #(#field_names:None),*
+                }
+            }
+        }
+    };
+
+
+    let builder_methods:Vec<_> = utils::derive_get_struct_fields(&ast).unwrap().iter().map(|field|{
+        let field_name = &field.ident;
+        let field_type = &field.ty;
+        quote! {
+            fn #field_name(&mut self, #field_name: #field_type) -> &mut Self {
+                self.#field_name = Some(#field_name);
+                self
+            }
+        }
+    }).collect();
+    
+
+    let builder_build_method = quote!(
+        fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+            #(
+                if let None = self.#field_names {
+                    return Err(#field_mising_err_msg.into())
+                }
+            )*
+            Ok(
+                #struct_name{
+                    #(#field_names:self.#field_names.clone().unwrap()),*
+                }
+            )
+        }
+    );
+
+
+
+    (quote!{
+        #builder_struct_def
+        impl #builder_struct_name {
+            #(#builder_methods)*
+            #builder_build_method
+        }
+    }).into()
 }
---
+++ builder/src/utils.rs
@@ -0,0 +1,18 @@
+use syn;
+
+pub fn derive_get_struct_fields(ast: &syn::DeriveInput) -> Option<&syn::punctuated::Punctuated<syn::Field, syn::Token![,]>>{
+    if let syn::Data::Struct(
+        syn::DataStruct{
+            fields: syn::Fields::Named(
+                syn::FieldsNamed{
+                    ref named,
+                    ..
+                }
+            ),
+            ..
+        }
+    ) = ast.data {
+       return Some(named)
+    }
+    None
+}
