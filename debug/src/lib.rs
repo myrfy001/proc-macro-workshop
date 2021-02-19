90ede1f49e70b94f99f3aa06361dd7c0ab309c91
myrfy001 <myrfy001@users.noreply.github.com>
2021-02-19 21:58:32 +0800

    debug-08-finished

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -5,13 +5,13 @@ use syn::{DeriveInput, Result, parse_macro_input, parse_quote};
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(input as DeriveInput);
-    // eprintln!("{:#?}", ast);
-    do_derive(&ast).unwrap_or_else(syn::Error::into_compile_error).into()
+    let st = parse_macro_input!(input as DeriveInput);
+    // eprintln!("{:#?}", st);
+    do_derive(&st).unwrap_or_else(syn::Error::into_compile_error).into()
 }
 
 
-fn get_all_struct_fields(ast: &DeriveInput) -> Option<&syn::punctuated::Punctuated<syn::Field, syn::Token![,]>>{
+fn get_all_struct_fields(st: &DeriveInput) -> Option<&syn::punctuated::Punctuated<syn::Field, syn::Token![,]>>{
     if let syn::Data::Struct(
         syn::DataStruct{
             fields: syn::Fields::Named(
@@ -22,7 +22,7 @@ fn get_all_struct_fields(ast: &DeriveInput) -> Option<&syn::punctuated::Punctuat
             ),
             ..
         }
-    ) = ast.data {
+    ) = st.data {
         return Some(named)
     }
     None
@@ -41,10 +41,25 @@ fn get_inert_attribute_of_field(field:&syn::Field) -> Option<String> {
     None
 }
 
-fn inject_trait_bound(ast: &DeriveInput) -> syn::Generics{
-    let mut g = ast.generics.clone();
+fn get_struct_escape_hatch(st: &DeriveInput) -> Option<String> {
+    if let Some(inert_attr)  = st.attrs.last(){
+        if let Ok(syn::Meta::List(syn::MetaList{nested, ..})) = inert_attr.parse_meta(){
+            if let Some(syn::NestedMeta::Meta(syn::Meta::NameValue(path_value))) = nested.last() {
+                if path_value.path.is_ident("bound") {
+                    if let syn::Lit::Str(ref lit) = path_value.lit{
+                        return Some(lit.value())
+                    }
+                }
+            }
+        }
+    }
+    None
+}
 
-    let all_phantomdata_generic_type_ident:Vec<_> = get_all_struct_fields(ast).as_ref().unwrap().iter().filter_map(|field|{
+fn inject_trait_bound(st: &DeriveInput) -> syn::Generics{
+    let mut g = st.generics.clone();
+
+    let all_phantomdata_generic_type_ident:Vec<_> = get_all_struct_fields(st).as_ref().unwrap().iter().filter_map(|field|{
         if let syn::Type::Path(syn::TypePath{ref path,..}) = field.ty {
             if let Some(ps) = path.segments.last() {
                 if ps.ident == "PhantomData" {
@@ -61,7 +76,7 @@ fn inject_trait_bound(ast: &DeriveInput) -> syn::Generics{
         None
     }).collect();
 
-    let all_associated_types:Vec<_> = get_all_struct_fields(ast).as_ref().unwrap().iter().filter_map(|field|{
+    let all_associated_types:Vec<_> = get_all_struct_fields(st).as_ref().unwrap().iter().filter_map(|field|{
         if let syn::Type::Path(syn::TypePath{ref path,..}) = field.ty {
             if let Some(ps) = path.segments.last() {
                 if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments{ref args, ..}) = ps.arguments {
@@ -104,11 +119,19 @@ fn inject_trait_bound(ast: &DeriveInput) -> syn::Generics{
     g
 }
 
-fn do_derive(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
-    let target_struct_name_ident = &ast.ident;
+fn inject_escape_hatch(st: &DeriveInput, hatch_lit: String) -> syn::Generics{
+    let mut g = st.generics.clone();
+    let wc = g.make_where_clause();
+    let st_node = syn::parse_str(hatch_lit.as_str()).unwrap();
+    wc.predicates.push(st_node);
+    g
+}
+
+fn do_derive(st: &DeriveInput) -> Result<proc_macro2::TokenStream> {
+    let target_struct_name_ident = &st.ident;
     let target_struct_name_literal = target_struct_name_ident.to_string();
 
-    let default_fmt_body:Vec<_> = get_all_struct_fields(ast).unwrap().iter().map(|field|{
+    let default_fmt_body:Vec<_> = get_all_struct_fields(st).unwrap().iter().map(|field|{
         let field_name_ident = &field.ident;
         let field_name_literal = field_name_ident.as_ref().unwrap().to_string();
 
@@ -124,7 +147,14 @@ fn do_derive(ast: &DeriveInput) -> Result<proc_macro2::TokenStream> {
         
     }).collect();
 
-    let expanded_generics = inject_trait_bound(ast);
+
+
+    let expanded_generics;
+    if let Some(hatch_lit) = get_struct_escape_hatch(st) {
+        expanded_generics = inject_escape_hatch(st, hatch_lit);
+    } else {
+        expanded_generics = inject_trait_bound(st);
+    }
 
     let (impl_generics, ty_generics, where_clause) = expanded_generics.split_for_impl();
 
--- main.rs
+++ main.rs
@@ -11,9 +11,7 @@
 
 
 
-use derive_builder::Builder;
 use derive_debug::CustomDebug;
-
 use std::fmt::Debug;
 
 pub trait Trait {
@@ -21,19 +19,24 @@ pub trait Trait {
 }
 
 #[derive(CustomDebug)]
-pub struct Field<T: Trait> {
+#[debug(bound = "T::Value: Debug")]
+pub struct Wrapper<T: Trait> {
+    field: Field<T>,
+}
+
+#[derive(CustomDebug)]
+struct Field<T: Trait> {
     values: Vec<T::Value>,
 }
 
 fn assert_debug<F: Debug>() {}
 
 fn main() {
-    // Does not implement Debug, but its associated type does.
     struct Id;
 
     impl Trait for Id {
         type Value = u8;
     }
 
-    assert_debug::<Field<Id>>();
+    assert_debug::<Wrapper<Id>>();
 }
